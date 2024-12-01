8b099924042eaf52c7c77ab445e95eb402d77281
Matheus-Chizzolini <matchizzolini@gmail.com>
2024-12-01 13:01:00 -0300

    att

--- src/componentes/Telas/TelaCadastroProduto.jsx
+++ src/componentes/Telas/TelaCadastroProduto.jsx
@@ -1,56 +1,51 @@
 import { Alert } from "react-bootstrap";
 import FormCadProdutos from "./Formularios/FormCadProduto";
 import Pagina from "../layouts/Pagina";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import TabelaProdutos from "./Tabelas/TabelaProdutos";
 //import { produtos } from "../../dados/mockProdutos";
+
 
 export default function TelaCadastroProduto(props) {
     const [exibirTabela, setExibirTabela] = useState(true);
     const [modoEdicao, setModoEdicao] = useState(false);
     //const [produtos, setProdutos] = useState([]);
     const [produtoSelecionado, setProdutoSelecionado] = useState({
-        codigo: 0,
-        descricao: "",
-        precoCusto: 0,
-        precoVenda: 0,
-        qtdEstoque: 0,
-        urlImagem: "",
-        dataValidade: "",
+        codigo:0,
+        descricao:"",
+        precoCusto:0,
+        precoVenda:0,
+        qtdEstoque:0,
+        urlImagem:"",
+        dataValidade:"",
         categoria: {}
 
     });
 
-    useEffect(() => {
-        consultarProduto().then((lista) => {
-            setListaDeProdutos(lista);
-        });
-    }, []); //listaVazia -> didMount
-
+  
     return (
         <div>
             <Pagina>
-                <Alert className="mt-02 mb-02 success text-center" variant="success">
+                |<Alert className="mt-02 mb-02 success text-center" variant="success">
                     <h2>
                         Cadastro de Produto
                     </h2>
                 </Alert>
                 {
                     exibirTabela ?
-                        <TabelaProdutos
-                            setExibirTabela={setExibirTabela}
-                            setModoEdicao={setModoEdicao}
-                            setProdutoSelecionado={setProdutoSelecionado} 
-                        /> :
-                        <FormCadProdutos
-                            setExibirTabela={setExibirTabela}
-                            produtoSelecionado={produtoSelecionado}
-                            setProdutoSelecionado={setProdutoSelecionado}
-                            modoEdicao={modoEdicao}
-                            setModoEdicao={setModoEdicao}
-                        />
+                        <TabelaProdutos setExibirTabela={setExibirTabela}
+                                        setModoEdicao={setModoEdicao}
+                                        setProdutoSelecionado={setProdutoSelecionado} /> :
+                        <FormCadProdutos setExibirTabela={setExibirTabela}
+                                         produtoSelecionado={produtoSelecionado}
+                                         setProdutoSelecionado={setProdutoSelecionado}
+                                         modoEdicao={modoEdicao}
+                                         setModoEdicao={setModoEdicao}
+
+                                         />
                 }
             </Pagina>
         </div>
     );
+
 }
--- src/redux/produtoReducer.js
+++ src/redux/produtoReducer.js
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import ESTADO from "./estados";
-import { consultarProduto } from "../servicos/servicoProduto";
+import { consultarProduto, excluirProduto as excluir } from "../servicos/servicoProduto";
 
-const buscarProdutos = createAsyncThunk('buscarProdutos', async () => {
+export const buscarProdutos = createAsyncThunk('buscarProdutos', async () => {
     const resultado = await consultarProduto();
 
     try {
@@ -11,7 +11,7 @@ const buscarProdutos = createAsyncThunk('buscarProdutos', async () => {
             return {
                 "status": true,
                 "mensagem": "Produtos recuperados com sucesso.",
-                listaDeProdutos
+                "listaDeProdutos": resultado
             }
         }
         else {
@@ -29,8 +29,27 @@ const buscarProdutos = createAsyncThunk('buscarProdutos', async () => {
             "listaDeProdutos": []
         }
     }
-    
+
 });
+
+export const excluirProduto = createAsyncThunk('excluirProduto', async (produto) => {
+    console.log(produto);
+    const resultado = await excluir(produto);
+    console.log(resultado);
+    try {
+        return {
+            "status": resultado.status,
+            "mensagem": resultado.mensagem,
+        }
+    }
+    catch (erro) {
+        return {
+            "status": false,
+            "mensagem": "Erro: " + erro.message,
+        }
+    }
+});
+
 const produtoReducer = createSlice({
     name: 'produto',
     initialState: {
@@ -44,23 +63,35 @@ const produtoReducer = createSlice({
             state.estado = ESTADO.PENDENTE
             state.mensagem = "Processando requisição (buscando produtos)"
         })
-        .addCase(buscarProdutos.fulfilled, (state, action) => {
-            if (action.payload.status) {
-                state.estado = ESTADO.OCIOSO;
-                state.mensagem = action.payload.mensagem;
-                state.listaDeProdutos = action.payload.listaDeProdutos;
-            }
-            else {
+            .addCase(buscarProdutos.fulfilled, (state, action) => {
+                if (action.payload.status) {
+                    state.estado = ESTADO.OCIOSO;
+                    state.mensagem = action.payload.mensagem;
+                    state.listaDeProdutos = action.payload.listaDeProdutos;
+                }
+                else {
+                    state.estado = ESTADO.ERRO;
+                    state.mensagem = action.payload.mensagem;
+                    state.listaDeProdutos = action.payload.listaDeProdutos;
+                }
+            })
+            .addCase(buscarProdutos.rejected, (state, action) => {
                 state.estado = ESTADO.ERRO;
                 state.mensagem = action.payload.mensagem;
                 state.listaDeProdutos = action.payload.listaDeProdutos;
-            }
-        })
-        .addCase(buscarProdutos.rejected, (state, action) => {
-            state.estado = ESTADO.ERRO;
-            state.mensagem = action.payload.mensagem;
-            state.listaDeProdutos = action.payload.listaDeProdutos;
-        })
+            })
+            .addCase(excluirProduto.pending, (state, action) => { //
+                state.estado = ESTADO.PENDENTE;
+                state.mensagem = "Processando a requisição"; //
+            })
+            .addCase(excluirProduto.fulfilled, (state, action) => {
+                state.estado = ESTADO.OCIOSO;
+                state.mensagem = action.payload.mensagem;
+            })
+            .addCase(excluirProduto.rejected, (state, action) => {
+                state.estado = ESTADO.ERRO;
+                state.mensagem = ""
+            })
     }
 })
 
